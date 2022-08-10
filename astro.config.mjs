ec5c7f3d109d6d98d0e5651b552a4afa9b62f45d
AndreaPontrandolfo <andrea.pontra@gmail.com>
2022-08-11 00:07:47 +0200

    added some astro plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import astro from "astro-compress";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: { integrations: true },
+  integrations: [astro(), mdx(), sitemap(), tailwind()],
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
