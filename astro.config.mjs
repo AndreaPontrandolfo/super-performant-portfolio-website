9c68309f9f0d08f631729a9cfbaadfc7559eaf50
AndreaPontrandolfo <andrea.pontra@gmail.com>
2022-08-12 15:28:24 +0200

    feat: changed layout implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import { astroImageTools } from "astro-imagetools";
 import astro from "astro-compress";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
@@ -7,5 +8,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   experimental: { integrations: true },
-  integrations: [astro(), mdx(), sitemap(), tailwind()],
+  integrations: [astro(), mdx(), sitemap(), tailwind(), astroImageTools],
 });
