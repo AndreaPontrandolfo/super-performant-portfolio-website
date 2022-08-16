a2869e5f96d275872c81b4c002ff313b6771b445
AndreaPontrandolfo <andrea.pontrau@gmail.com>
2022-08-16 22:08:56 +0200

    added paintings gallery

--- src/utils/sharedPictureProps.js
+++ src/utils/sharedPictureProps.js
@@ -4,9 +4,12 @@ export const sharedPictureProps = {
   fallbackFormat: "jpg",
   includeSourceFormat: false,
   attributes: {
+    picture: {
+      class: "mt-1 mb-1",
+    },
     img: {
       //  "border-2 border-zinc-200" - consider adding this
-      class: "mt-2 rounded-md",
+      class: "rounded-md",
     },
   },
 };
