214fc4746d4efe77bc43943c812659f70f9e22e2
AndreaPontrandolfo <andrea.pontrau@gmail.com>
2022-08-16 15:20:28 +0200

    added venice series

---
+++ src/utils/sharedPictureProps.js
@@ -0,0 +1,12 @@
+export const sharedPictureProps = {
+  flatten: true,
+  preload: "webp",
+  fallbackFormat: "jpg",
+  includeSourceFormat: false,
+  attributes: {
+    img: {
+      //  "border-2 border-zinc-200" - consider adding this
+      class: "mt-2 rounded-md",
+    },
+  },
+};
