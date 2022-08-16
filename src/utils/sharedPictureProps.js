c5387c8e92ca0775ee0d46d86d2458412d8f0278
AndreaPontrandolfo <andrea.pontrau@gmail.com>
2022-08-17 01:58:21 +0200

    added more socials

--- src/utils/sharedPictureProps.js
+++ src/utils/sharedPictureProps.js
@@ -3,13 +3,4 @@ export const sharedPictureProps = {
   preload: "webp",
   fallbackFormat: "jpg",
   includeSourceFormat: false,
-  attributes: {
-    // picture: {
-    //   class: "mt-1 mb-1",
-    // },
-    img: {
-      //  "border-2 border-zinc-200" - consider adding this
-      // class: "rounded-md",
-    },
-  },
 };
