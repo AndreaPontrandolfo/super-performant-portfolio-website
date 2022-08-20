280b74e0e399144df0986a456793064ca18c0bac
AndreaPontrandolfo <andrea.pontra@gmail.com>
2022-08-20 17:50:03 +0200

    addede astro-seo-meta

--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -1,1 +1,9 @@
 export const WEBSITE_URL = "https://www.lucapontrandolfo.com";
+export const routes = {
+  INDEX: "/",
+  PAINTINGS: "/paintings",
+  TURIN: "/turin-series",
+  VENICE: "/venice-series",
+  CONTACTS: "/contacts",
+  ABOUT: "/about",
+};
